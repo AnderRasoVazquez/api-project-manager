1829884c539f4ab2979bb13e1ad66076e5fad4dd
Ander Raso Vázquez <anderraso@gmail.com>
2019-03-23 18:35:42 +0100

    work usa el decorator load y los validators de model
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

--- work.py
+++ work.py
@@ -1,9 +1,6 @@
 from flask import Blueprint, jsonify, request
-from model import works_schema, work_schema, Task, db, Work
-from decorators import token_required
-from cerberus import Validator
-import datetime
-import json
+from model import works_schema, work_schema, Task, db, Work, create_work_validator, update_work_validator
+from decorators import token_required, load_data
 
 
 work_api = Blueprint('work_api', __name__)
@@ -61,13 +58,9 @@ def delete_work(current_user, work_id):
 
 @work_api.route('/api/v1/tasks/<task_id>/works', methods=['POST'])
 @token_required
-def create_work(current_user, task_id):
+@load_data
+def create_work(data, current_user, task_id):
     """Crea un dia trabajado."""
-    try:
-        data = json.loads(request.data)
-    except:
-        return jsonify({'message': 'Bad request'}), 400
-
     task = Task.query.filter_by(task_id=task_id).first()
 
     if not task:
@@ -76,15 +69,7 @@ def create_work(current_user, task_id):
     if current_user not in task.project.members:
         return jsonify({'message': 'You don\'t have permission to edit this task!'}), 403
 
-    to_date = lambda s: datetime.datetime.strptime(s, '%Y-%m-%d')
-    schema = {
-        'date': {'type': 'date', 'empty': False, 'coerce': to_date, 'required': True},
-        'time': {'type': 'float', 'empty': False, 'required': True}
-    }
-
-    validator = Validator(schema)
-
-    if validator.validate(data):
+    if create_work_validator.validate(data):
         if Work.query.filter_by(date=data['date']).first():
             return jsonify({'message': 'There\'s already work on that date!'}), 400
         else:
@@ -95,18 +80,14 @@ def create_work(current_user, task_id):
             db.session.commit()
             return jsonify({'message': 'Work created!', 'work': work_schema.dump(work).data}), 201
     else:
-        return jsonify({'message': 'Work not created!', 'errors': validator.errors}), 400
+        return jsonify({'message': 'Work not created!', 'errors': create_work_validator.errors}), 400
 
 
 @work_api.route('/api/v1/works/<work_id>', methods=['POST'])
 @token_required
-def modify_work(current_user, work_id):
+@load_data
+def modify_work(data, current_user, work_id):
     """Modifica un dia trabajado."""
-    try:
-        data = json.loads(request.data)
-    except:
-        return jsonify({'message': 'Bad request'}), 400
-
     work = Work.query.filter_by(work_id=work_id).first()
 
     if not work:
@@ -115,15 +96,9 @@ def modify_work(current_user, work_id):
     if current_user.user_id != work.user_id:
         return jsonify({'message': 'You don\'t have permission to edit this work!'}), 403
 
-    schema = {
-        'time': {'type': 'float', 'empty': False, 'required': True}
-    }
-
-    validator = Validator(schema)
-
-    if validator.validate(data):
+    if update_work_validator.validate(data):
         work.time = data["time"]
         db.session.commit()
         return jsonify({'message': 'Work modified!', 'work': work_schema.dump(work).data})
     else:
-        return jsonify({'message': 'Work not created!', 'errors': validator.errors}), 400
+        return jsonify({'message': 'Work not created!', 'errors': update_work_validator.errors}), 400
